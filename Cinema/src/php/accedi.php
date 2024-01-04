cdecbdbd7307e1bee582e88fc625bc3331ac6437
MarcoCappellari <marco.cap.blu@gmail.com>
2024-01-04 18:34:52 +0100

    migliorata grafica login

--- Cinema/src/php/accedi.php
+++ Cinema/src/php/accedi.php
@@ -1,10 +1,9 @@
 <?php
 
 require_once '../../queries/queries.php';
-session_start(); //serve per usare $_SESSION['username'] (avvio la sessione)
+session_start();
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    // Verifica se i campi sono stati inviati
     if (!isset($_POST['username']) || !isset($_POST['password'])) {
         header('Location: ../html/404.html');
         exit();
@@ -14,16 +13,20 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $password = $_POST['password'];
 
     $user = getUserByMailOrUsername($conn, $username);
+
     $conn->close();
-    // Verifica se l'utente esiste e la password corrisponde
-    if ($password == $user['password'] && $username == $user['username']) {
-        $_SESSION['username'] = $user['username'];
+
+    if ($user === null) {
+        echo "Utente non trovato.";
+        exit();
+    }
+
+    if ($password == $user['password'] && ($username == $user['username'] || $username == $user['mail'])) {
         echo "Credenziali corrette.";
-        exit(); // Interrompi lo script dopo il reindirizzamento
+        exit();
     } else {
         echo "Credenziali errate o password non corretta.";
     }
 }
-
 
 ?>
