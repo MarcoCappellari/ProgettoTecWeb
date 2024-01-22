825b2728b149f90717aa94cf0b06a007bb195d5c
MarcoCappellari <marco.cap.blu@gmail.com>
2024-01-22 21:02:40 +0100

    modificata grafica + aggiornati tutti i link

--- Cinema/queries/connessione_database.php
+++ Cinema/queries/connessione_database.php
@@ -3,7 +3,7 @@
     $servername = "localhost";
     $username = "root";
     $password = "";
-    $dbname = "cinema";
+    $dbname = "cinema6";
 
     try {
         $conn = new mysqli($servername, $username, $password, $dbname);
--- Cinema/src/php/recensioni.php
+++ Cinema/src/php/recensioni.php
@@ -7,7 +7,8 @@ session_start();
 $recensioni_page = file_get_contents('../html/recensioni.html');
 if (isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true) {
     $accedi_stringa = "Benvenuto " . $_SESSION['username'];
-    if($_SESSION['conferma-recensione'] == true){
+    
+    if (isset($_SESSION['conferma-recensione']) && $_SESSION['conferma-recensione'] == true) {
         $_SESSION['conferma-recensione'] = false;
         $conferma_recensione = "<p>Recensione inviata con successo!<br>
                                 Vuoi scrivere un'altra recensione? <a href='recensioni.php'>Clicca qui</a>!</p>";
@@ -21,7 +22,7 @@ if (isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true) {
     }
 } else {
     $accedi_stringa = "<a href='../html/accedi.html'>Accedi</a>";
-    $accedi_per_recensione =    "<p>Ops! Sembra che tu non abbia ancora effettuato l'accesso!<br>
+    $accedi_per_recensione = "<p>Ops! Sembra che tu non abbia ancora effettuato l'accesso!<br>
                                 Per lasciare una recensione devi prima aver effettuato l'accesso al tuo accout.<br>
                                 Clicca qui per <a href='../html/accedi.html'>Accedere</a>.</p>";
     $recensioni_page = str_replace('{SCRIVI-RECENSIONE}', $accedi_per_recensione, $recensioni_page);
@@ -34,18 +35,18 @@ $conn->close();
 
 $output_recensioni = '';
 
-if(!$recensioni){
-    $output_recensioni='<div class="div-non-esiste">
+if (!$recensioni) {
+    $output_recensioni = '<div class="div-non-esiste">
                             <div class="immagine-indisponibilità"></div>
                             <p>E\' ancora tutto molto tranquillo qui!</p>
                             <p>Condividi per primo la tua esperienza scrivendo una recensione. La tua opinione conta!</p>
                         <div>';
 } else {
     while ($recensione = $recensioni->fetch_assoc()) {
-        $output_recensioni.='<div class="recensione">
-                                <p class="utente">'. $recensione['username'] .'</p>
-                                <p class="data-recensione"><time datatime="'.$recensione['data'].'">'. $recensione['data'] .'</time></p>
-                                <p class="content-recensione">"'. $recensione['content'] .'"</p> 
+        $output_recensioni .= '<div class="recensione">
+                                <p class="utente">' . $recensione['username'] . '</p>
+                                <p class="data-recensione"><time datatime="' . $recensione['data'] . '">' . $recensione['data'] . '</time></p>
+                                <p class="content-recensione">"' . $recensione['content'] . '"</p> 
                             </div>';
     }
 }
