64233206423e63c23d2cc258c30f530bca04dd2f
Menny <giovanni.menon15@gmail.com>
2024-01-27 12:12:15 +0100

    update

--- Cinema/src/php/prenota.php
+++ Cinema/src/php/prenota.php
@@ -6,6 +6,9 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+include 'user_session.php';
+$accedi_stringa = gestisciAccesso($conn);
+
 $id_film = $_POST['idFilm'];
 $ora_film = $_POST['ora'];
 $data_film = $_POST['data'];
@@ -18,7 +21,7 @@ if (empty($id_film) || empty($ora_film) || empty($data_film) || absentProiection
 
 $FilmRow = getFilmByIdQuery($conn, $id_film);
 $Sala = getSalaByProiection($conn, $id_film, $ora_film, $data_film);
-$conn->close();
+
 
 $titolo = $FilmRow['titolo'];
 $nome_sala= $Sala['nome'];
@@ -46,20 +49,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         }
 
         if (isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true) {
-            //$permessi = getPermessiByUsername($conn, $_SESSION['username']);
-            $permessi = false;
-            if ($permessi == true) {
-                $accedi_stringa = "<a href='admin.php'>Benvenuto " . $_SESSION['username'] . "</a>";
-            } else {
-                $accedi_stringa = "<a href='profilo.php'>Benvenuto " . $_SESSION['username'] . "</a>";
-            }
-            $registrazione = '<h2>Acquista i biglietti come '.$_SESSION['username'].'</h2>';
+            $registrazione = '<h2>Acquista i biglietti come '.$_SESSION['mail'].'</h2>';
 
             $prenota .= '<input type="hidden" name="idFilm" value="' . $id_film . '">';
             $prenota .= '<input type="hidden" name="data" value="' . $data_film . '">';
             $prenota .= '<input type="hidden" name="ora" value="' . $ora_film . '">';
             $prenota .= '<input type="hidden" name="sala" value="' . $nome_sala . '">';
-            $prenota .= '<input type="hidden" name="posti" value="' . $posti_selezionati . '">';
+            $prenota .= '<input type="hidden" name="posti" value="'. htmlspecialchars(json_encode($posti_selezionati)) .'">';
 
         } else {
             $registrazione = '<h2>Acquista i biglietti senza registrarti</h2>';
@@ -81,7 +77,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         exit;
     }
 }
-
+$conn->close();
 
 $ora_codificata = urlencode($ora_film);
 
@@ -97,8 +93,6 @@ $template = str_replace('{TITOLO}', $titolo, $template);
 $template = str_replace('{DATA}', $data_film, $template);
 $template = str_replace('{ORA}', $ora_codificata, $template);
 $template = str_replace('{SALA}', $nome_sala, $template);
-
-
 
 $stringa_footer= file_get_contents('../html/footer.html');
 $template = str_replace('{FOOTER}', $stringa_footer, $template);
