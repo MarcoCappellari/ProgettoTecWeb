37b84660c7dd2f97a2609de724ecf6cbc9a36c4d
Menny <giovanni.menon.dev@gmail.com>
2024-01-28 16:44:07 +0100

    update

--- Cinema/src/php/registrazione.php
+++ Cinema/src/php/registrazione.php
@@ -3,44 +3,44 @@
 require_once '../../queries/queries.php';
 session_start();
 
+$risultato_info = '';
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    if (!isset($_POST['utente']) || !isset($_POST['password'])) {
-        header('Location: ../html/404.html');
-        exit();
-    }
-
-    $utente = $_POST['utente'];
+    $check_cont = 0;
+    $email = $_POST['email'];
+    $username = $_POST['utente'];
     $password = $_POST['password'];
+    $password_conferma = $_POST['password_conferma'];
+    $nome = $_POST['nome'];
+    $cognome = $_POST['cognome'];
 
-    $user = getUserByMailOrUsername($conn, $utente);
-
-    if ($user === null) {
-        echo "Utente non trovato.";
-        exit();
+    $check_1 = getUserByMailOrUsername($conn, $username);
+    if ($check_1 !== null) {
+        $risultato_info =  "<p>Esiste gia' un utente con questo Username</p>";
+        $check_cont = 1;
     }
 
-    //dovrebbe essere inutile, basta $username = $user['username'];
-    if ($utente == $user['mail']) {
-        $username = getUserByMail($conn, $utente);
-    } else {
-        $username = $user['username'];
+    $check_2 = getUserByMailOrUsername($conn, $email);
+    if ($check_1 !== null) {
+        $risultato_info = "<p>Esiste gia' un utente con questo Email</p>";
+        $check_cont = 1;
     }
 
+    if($password !== $password_conferma ){
+        $risultato_info =  "<p>Le password non corrispondono</p>";
+        $check_cont = 1;
+    }
+
+    if($check_cont === 0){
+        $sql = "INSERT INTO Utente (mail, username, nome, cognome, permessi, password) VALUES ('$email','$username','$nome','$cognome',0,'$password')";
+        $conn->query($sql);
+        $risultato_info='<p>Utente registrato con SUCCESSO!</p>';
+    }
     $conn->close();
-
-    if ($password == $user['password'] && $username == $user['username']) {
-        echo "Credenziali corrette.";
-        $_SESSION['mail-utente']= $user['mail'];
-        $_SESSION['username'] = $username; //$user['username'] posso sostituirlo
-        $_SESSION['logged_in'] = true;
-        header('Location: ../../index.php');
-        exit();
-    } else {
-        echo "Credenziali errate o password non corretta.";
-    }
 }
 
 $template = file_get_contents('../html/registrazione.html');
+
+$template = str_replace('{FEEDBACK}', $risultato_info, $template);
 echo $template;
 
 ?>
