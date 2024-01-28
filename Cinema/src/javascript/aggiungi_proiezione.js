915f5036419249118c67abec7654b1ef55a72563
MarcoCappellari <marco.cap.blu@gmail.com>
2024-01-28 14:39:55 +0100

    aggiunto JS per aggiungi_proiezione

--- Cinema/src/javascript/aggiungi_proiezione.js
+++ Cinema/src/javascript/aggiungi_proiezione.js
@@ -1,6 +1,6 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     var form = document.getElementById("aggiunta-proiezione-form");
-    form.addEventListener("submit", function(event) {
+    form.addEventListener("submit", function (event) {
         // Resetta eventuali errori precedenti
         resettaErrori();
 
@@ -9,14 +9,39 @@ document.addEventListener("DOMContentLoaded", function() {
         if (!validaData(data.value)) {
             mostraErrore(data, "La data non è valida.");
             event.preventDefault(); // Impedisce l'invio del modulo se la data non è valida
+            return; // Esce dalla funzione per evitare ulteriori controlli
+        }
+
+        // Verifica la convalida dell'ora
+        var ora = document.getElementById("ora");
+        if (!validaOra(ora.value)) {
+            mostraErrore(ora, "L'ora non è valida.");
+            event.preventDefault(); // Impedisce l'invio del modulo se l'ora non è valida
+            return; // Esce dalla funzione per evitare ulteriori controlli
         }
     });
 
     function validaData(dataString) {
         var dataInput = new Date(dataString);
         var oggi = new Date();
-        return dataInput >= oggi; // La data deve essere uguale o successiva a oggi
+        oggi.setDate(oggi.getDate() - 1);
+        return dataInput >= oggi;
     }
+
+    function validaOra(oraString) {
+        // Separare l'ora e i minuti
+        var oreMinuti = oraString.split(':');
+        var ore = parseInt(oreMinuti[0], 10);
+        var minuti = parseInt(oreMinuti[1], 10);
+
+        // Controlla se le ore sono comprese tra 0 e 23 e se i minuti sono compresi tra 0 e 59
+        if (ore >= 0 && ore <= 23 && minuti >= 0 && minuti <= 59) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 
     function mostraErrore(input, messaggio) {
         var errore = document.createElement("p");
